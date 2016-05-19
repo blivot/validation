29c4ee715704e0e228366838fb8b08844a82db3f
Ethan Mick <ethan@ethanmick.com>
2016-05-19 18:13:52 -0400

    Boolean

--- src/main/java/com/virtyx/validation/ValidationBoolean.java
+++ src/main/java/com/virtyx/validation/ValidationBoolean.java
@@ -1,5 +1,9 @@
 package com.virtyx.validation;
 
-public class ValidationBoolean {
-
+public class ValidationBoolean extends ValidationAny<Boolean, ValidationBoolean> {
+	
+	@Override
+	protected ValidationBoolean getThis() {
+		return this;
+	}
 }
